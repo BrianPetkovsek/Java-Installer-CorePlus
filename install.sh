2deaf208f01138ca54b275d76491a2912acc41ec
Brian Petkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2020-12-22 17:41:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@ tce-load -iw openssl
 tce-load -iw python
 
 mkdir /tmp/java-install-script
-wget https://docs.google.com/document/d/1vGjycNCq3L14SbTRYY8AHGUmo_7Xqji4w2kuEkrcAhQ/export?format=html -O /tmp/java-install-script/tmp.file
+wget https://docs.google.com/document/d/1vGjycNCq3L14SbTRYY8AHGUmo_7Xqji4w2kuEkrcAhQ/export?format=txt -O /tmp/java-install-script/tmp.file
 wget https://github.com/BrianPetkovsek/Java-Installer-CorePlus/raw/main/gdrivel.py -O gdrivel.py
-python gdrivel.py -q $(cat /tmp/java-install-script/tmp.file)
+python gdrivel.py -q $(cat /tmp/java-install-script/tmp.file) -P /tmp/java-install-script -O jdk-8u271-linux-i586.tar.gz
+
+rm -f /tmp/java-install-script/tmp.file
 
 tar -xzf /tmp/java-install-script/jdk-8u271-linux-i586.tar.gz
 
